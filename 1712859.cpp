b738eff6ffc1071a5c9b25e3de76cf0de260c57e
1712859 <38474247+1712859@users.noreply.github.com>
2018-04-18 23:17:34 +0700

    Add files via upload

---
+++ 1712859.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+struct SinhVien
+{
+	int MSSV;
+	char HoVaTen;
+	char Khoa;
+	int NamHoc;
+	NgaySinh Date;
+	char Hinh;
+	char MoTaBanThan;
+	char SoThich;
+};
+struct NgaySinh
+{
+	int ngay;
+	int thang;
+	int nam;
+};
+int main()
+{
+	string ex = System.Text.Encoding.UTF8.GetString(utf);
+	return 1;
+}
