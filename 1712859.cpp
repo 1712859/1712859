e949faf173aa35f28a94ea6959c19ecd1d314a26
1712859 <38474247+1712859@users.noreply.github.com>
2018-04-23 22:49:30 +0700

    Update 1712859.cpp

--- 1712859.cpp
+++ 1712859.cpp
@@ -1,40 +1,64 @@
 #include<stdio.h>
 #include<string>
+#include<wchar.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <io.h>
 using namespace std;
 struct SinhVien
 {
-	int MSSV;
-	wchar_t HoVaTen;
-	wchar_t Khoa;
-	int NamHoc;
-	wchar_t Date;
-	wchar_t Hinh;
-	wchar_t MoTaBanThan;
-	wchar_t SoThich[10];
-	wchar_t email;
+	wchar_t MSSV[10];
+	wchar_t HoVaTen[30];
+	wchar_t Khoa[50];
+	wchar_t NamHoc[4];
+	wchar_t Date[100];
+	wchar_t Hinh[100];
+	wchar_t MoTaBanThan[100];
+	wchar_t SoThich[1000];
+	wchar_t email[100];
 };
 typedef struct SinhVien SV;
-
-int KiemTraKetThuc(wchar_t c)
+int SLDong;
+void DemDong(FILE *fp, int &Sodong)
 {
-	return (c == '\n' || c == EOF);
+	char ch;
+	do
+	{
+		ch = fgetc(fp);
+		if (ch == '\n')
+		{
+			Sodong++;
+		}
+	} while (ch != EOF);
 }
-int DocDong(wchar_t * dong, int SLDong, FILE *fp)
+void docsinhvien(FILE *f, SV &a)
 {
-	wchar_t c;
-	int i = 0;
-	while (!KiemTraKetThuc(c = fgetc(fp)) && i < SLDong - 1)
-		dong[i++] = c;
-	dong[i] = 0; 
+	fscanf_s(f, "\" %[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\n]", &a.MSSV, &a.HoVaTen, &a.Khoa, &a.NamHoc, &a.Date, &a.Hinh, &a.MoTaBanThan, &a.email, &a.SoThich);
+}
+void DocFile(FILE *fp, SV a[])
+{
 
-	if (i == SLDong - 1) 
-	while ((c = fgetc(fp)) != '\n') 
-		;
-
-	return i; 
+	for (int i = 0; i < SLDong; i++)
+	{
+		docsinhvien(fp, a[i]);
+	}
 }
 int main()
 {
-	FILE *fp = fopen("list.csv", "r");
+	int i;
+	FILE *fp = _wfopen(L"list.csv", L"r , ccs = UTF - 8");
+	DemDong(fp, SLDong);
+	if (!fp)
+	{
+		wprintf(L"Mo file  that bai\n");
+		return 0;
+	}
+	SV a[10];
+	DocFile(fp, a);
+	for (i = 0; i < SLDong; i++)
+	{
+		wprintf(L"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", a[i].MSSV, a[i].HoVaTen, a[i].Khoa, a[i].NamHoc, a[i].Date, a[i].Hinh, a[i].MoTaBanThan, a[i].email, a[i].SoThich);
+	}
+	fclose(fp);
 	return 1;
 }
